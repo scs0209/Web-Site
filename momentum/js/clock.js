64e960ef365b6cc2026c65815f4999b037aa7956
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-09-01 01:07:03 +0900

    clock 만들기 완성!!

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,7 +1,15 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClcok() {
+  //Date 객체는 코드를 실행한 그 시점의 밀리초, 초, 시간 등을 알려준다.
+  const date = new Date();
+  //padStart(2,"0")=A의 글자수는 2글자이며 2글자가 아닐경우 앞에 "0"을 붙여준다.
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(sayHello, 5000);//첫번째 argument는 내가 실행하고자 하는 function, 두번째는 호출되는 function 간격을 몇 ms(milliseconds)로 할 지 씀
+
+getClcok();//시계가 즉시 실행하게 해주는 코드 없으면 0부터 시작한다.
+setInterval(getClcok, 1000);
