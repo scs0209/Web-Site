080524babd5b5bae9a3dba4396106557a2aa0ee7
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-08-31 16:02:47 +0900

    js를 이용하는 대신 html의 (div -> form)오로 교체해서 유용한 기능 사용

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,18 @@
-const h1 = document.querySelector(".title:first-child h1");
+//div id = login-form부분을 html에서 js로 가져옴
+// const loginForm = document.getElementById("login-form");
+//getElementById는 #을 사용할 필요가 없다 id인것을 이미 명시함
+//querySelector에서는 #이 id라는 뜻이디.
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
 
+//위의 코드와 똑같은 의미이다.
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+function loginBtn(){
+  const username = loginInput.value;
+  console.log(username);
 }
-//이전 버전보다 더 쉽게 코드를 수정한 버전
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", loginBtn);
+loginInput.addEventListener()
