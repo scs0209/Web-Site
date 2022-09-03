09656fea4091d42ef74d18ccd236b2f1571a77ac
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-09-03 17:29:33 +0900

    undefined가 자꾸 뜸

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -6,10 +6,13 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-let toDos = []; // (todo)에 들어오는 텍스트를 배열로 묶어 보관하기 위해 빈 array를 생성해준다.
+
+//데이터베이스에 To Do 내용을 추가하는 곳
+// (todo)에 들어오는 텍스트를 배열로 묶어 보관하기 위해 빈 array를 생성해준다.
+let toDos = [];
 
 function saveToDos () {
-  //JSON.stringify는 array자체를 문자열로 만들어준다.(localstorage에서 배열 형태로 저장됨.)
+  //JSON.stringify는 array자체를 문자열로 만들어준다.(localStorage에서 배열 형태로 저장됨.)
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
@@ -18,18 +21,26 @@ function saveToDos () {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  //클릭했던 li의 id를 갖고 있는 toDo를 지우는 코드
+  //즉, toDo의 id가 li의 id와 다른 걸 남기고 싶다는 뜻
+  /*toDo.id는 number 타입이고 li.id는 string 타입이기 때문에 변환해주지 않으면 실행 x 그래서 li.id를 number타입으로 변환해주는 코드 사용*/ 
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  //DB에서 to do를 지운 후 저장을 한 번 더 해줘야하기 때문에 사용
+  saveToDos(); 
 }
 
 
 //화면에 (표시)그리는 것을 담당
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
+  //원래는 paintToDo가 Text를 받았지만 이제는 object를 받기 때문에 .text가 붙는다.
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌"
   button.addEventListener("click", deleteToDo);
-  li.appendChild(span);//위에 span과 li를 만들었는데, 내가 하고싶은거 span잉 li안에 들어가는 거기 때문에 이 코드를 써서 span을 li에 넣어준다.
+  li.appendChild(span);//위에 span과 li를 만들었는데, 내가 하고싶은거 span이 li안에 들어가는 거기 때문에 이 코드를 써서 span을 li에 넣어준다.
   li.appendChild(button);
   toDoList.appendChild(li);
 }
@@ -39,8 +50,12 @@ function handleToDoSubmit(event){
   const newTodo = toDoInput.value;
   //text 상자 안에 글을 작성하고 enter를 눌렀을 때 그 글이 초기화 되게 만들기 위해 toDoInput.value = ""; 을 통해 value를 빈 텍스트로 한다.
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);//위에 있는 paintToDo 함수 호출
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };//id와 text를 생성해서 넣어줌
+  toDos.push(newTodoObj);//데이터베이스로 매번 사용자가 적어둔 객체를 push
+  paintToDo(newTodoObj);//위에 있는 paintToDo 함수 호출
   saveToDos();
 }
 
@@ -53,9 +68,7 @@ if(savedToDos !== null) {
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
 }
-
-//savedToDos에 있는 string을 parsedToDos에 불러와서 array형태로 변경한다.
-//JS는 배열안에 있는 각각의 item에 대해 function을 실행할 수 있게 해준다.
+//forEach함수는 이 paintToDo를 parsedToDos 배열의 요소마다 실행한다.
 
 
 //localStorage는 배열은 저장 x 오직 텍스트만 저장 가능
